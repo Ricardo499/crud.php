466a3021a06dabc7c3606961aa97175f3095367e
Ricardo499 <60673384+Ricardo499@users.noreply.github.com>
2021-11-01 23:53:56 -0700

    parte1

--- insertar.php
+++ insertar.php
@@ -1,12 +1,7 @@
 <?php
 include("./inc/settings.php");
 validar();
-/*print_r ($_POST)."<br>";
-echo $_POST ['identificador']."<br>\n";
-echo $_POST ['nombre']."<br>\n";
-echo $_POST ['fecha']."<br>\n";
-echo $_POST ['numero']."<br>\n";
-echo $_POST ['numdouble']."<br>\n";*/
+
 
 $identificador=$_POST ['identificador'];
 $nombre=$_POST ['nombre'];
@@ -15,23 +10,26 @@ $numero=$_POST ['numero'];
 $numdouble=$_POST ['numdouble'];
 
 $query="INSERT INTO table1 (column1, column2, column3, column4, column5) VALUES ($identificador, '$nombre', '$fecha', $numero, $numdouble);";
-//echo $query;
-
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare($query);
+$stmt->bind_param(":id", $identificador);
+$stmt->bind_param(":nombre", $nombre);
+$stmt->bind_param(":fecha", $fecha);
+$stmt->bind_param(":numero", $numero);
+$stmt->bind_param(":numdouble", $numdouble);
+$stmt->execute();
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-if ( $conn->query($query)== TRUE){
+if ($stmt->num_rows() > 0){
     header("location:crud.php");
-}else{
-    echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
-
 }
-
+else{
+    echo "Algo salio mal <a href='https://localhost/crud.php'> clic aqui para volver al crud</a>" ;
+}
 
 ?>
