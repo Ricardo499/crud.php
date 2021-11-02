3385f529d01c1d8db5dfd147ede6e44ed2005507
Ricardo499 <60673384+Ricardo499@users.noreply.github.com>
2021-11-01 23:50:51 -0700

    parte1

--- eliminar.php
+++ eliminar.php
@@ -6,25 +6,21 @@ validar();
 print_r($_POST);
 
 $id=$_POST['colum1'];
-
-$query="DELETE FROM table1 WHERE column1=$id;";
-
-// echo $query;
-
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare($query);
+$stmt->bind_param(":id", $id);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-if ( $conn->query($query)== TRUE){
+$stmt->execute();
+if ($stmt->num_rows() > 0){
     header("location:crud.php");
 }else{
-    echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
+    echo "Algo salio mal <a href='https://localhost/crud.php'> clic aqui para volver al crud</a>" ;
 
 }
-
 
 ?>
